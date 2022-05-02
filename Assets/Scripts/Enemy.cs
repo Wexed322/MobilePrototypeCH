838c0b66d60be4f23533c91e347d68e7e2030443
JostiN322 <jostinoriguela@gmail.com>
2022-05-02 10:46:24 -0500

    commit de inicio

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyType {Fuerza, Magia, Agilidad}
+public class Enemy : MonoBehaviour
+{
+    Player referencePlayer;
+    public bool vulnerable;
+    public float damage;
+    public float vida;
+    public EnemyType myType;
+    //BOMBAS DE HUMO, BLUR
+    //PODER DE REGRESAR EL TIEMPO
+    //HACER ABUELO AL RIVAL Y REDUCIR SUS STATS
+
+
+    void Start()
+    {
+        referencePlayer = FindObjectOfType<Player>();
+        referencePlayer.currentEnemyReference = this;
+
+        this.GetComponent<Animator>().speed = 0.5f;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) 
+        {
+            this.GetComponent<Animator>().speed++;
+        }
+        
+    }
+
+    public void setVurnerability() 
+    {
+        vulnerable = !vulnerable;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour
+{
+    public testScriptable testScriptable_;
+    public testScriptable testScriptable_2;
+    public Enemy currentEnemyReference;
+    public float damage;
+    public float vida;
+    public float currentCoins;
+    public float currentDiamonds;
+    //ARMA
+    //STATS DE VELOCIDAD DE ATAQUE??, MAS VIDA, POWER UP
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) 
+        {
+            if (currentEnemyReference.vulnerable) 
+            {
+                Debug.Log("daño");
+            }
+        }
+
+        Debug.Log(SceneManager.GetActiveScene().buildIndex);
+        if (SceneManager.GetActiveScene().buildIndex == 0) 
+        {
+            testScriptable_ = testScriptable_2;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) 
+        {
+            testScriptable_.muertes = SceneManager.GetActiveScene().buildIndex + 1;
+            testScriptable_.vida -= SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            testScriptable_.muertes = SceneManager.GetActiveScene().buildIndex - 1;
+            testScriptable_.vida += SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+    }
+}
