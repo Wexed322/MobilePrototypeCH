39260076183788b3776949d29ccd6ba3798edac2
JostiN322 <jostinoriguela@gmail.com>
2022-06-21 09:48:58 -0500

    RandomDesicion

---
+++ Assets/Scripts/RandomDesicion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum EnumAttack { attack1, attack2, attack3, attack4 }
+public class GetRandomEnum<T>
+{
+    static System.Random _R = new System.Random();
+
+    public static T RandomEnumValue()
+    {
+        var v = Enum.GetValues(typeof(T));
+        Debug.Log(v);
+        return (T)v.GetValue(_R.Next(v.Length));
+    }
+
+}
